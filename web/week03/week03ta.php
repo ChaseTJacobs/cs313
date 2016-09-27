bd60dbe33e8385a2f28f961f4bd466aa9e229e74
Chase Jacobs <chasejacobs@MacBook-Pro.local>
2016-09-27 17:37:52 -0600

    dos

--- web/week03/week03ta.php
+++ web/week03/week03ta.php
@@ -3,7 +3,6 @@
 
 Name: <?php echo $_POST["name"]; ?><br>
 Email: <?php echo $_POST["email"]; ?><br>
-Major: <?php echo $_POST["major"]; ?><br>
 Places you have been: <br>
     <?php 
         if(!empty($POST['checkbox'])) {
