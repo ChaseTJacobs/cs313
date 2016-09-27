aee55ed099d74b075cb479e132fb7ea8020ca43f
Chase Jacobs <chasejacobs@MacBook-Pro.local>
2016-09-27 17:36:03 -0600

    checkboxes

--- web/week03/week03ta.php
+++ web/week03/week03ta.php
@@ -1,9 +1,18 @@
 <html>
 <body>
 
-Name: <?php echo $_POST["name"]; ?>
-Email: <?php echo $_POST["email"]; ?>
-
+Name: <?php echo $_POST["name"]; ?><br>
+Email: <?php echo $_POST["email"]; ?><br>
+Major: <?php echo $_POST["major"]; ?><br>
+Places you have been: <br>
+    <?php 
+        if(!empty($POST['checkbox'])) {
+            foreach($POST['checkbox'] as $check) {
+                echo $check;
+            }
+        }
+    ?><br>
+    <?php echo $_POST["comments"]; ?>
 
 </body>
 </html
