c3ced1237448dc1867bef777b869f460c02788eb
Chase Jacobs <chasejacobs@MacBook-Pro.local>
2016-09-27 20:28:58 -0600

    new

--- web/week03/week03ta.php
+++ web/week03/week03ta.php
@@ -2,7 +2,7 @@
 <body>
 
 Name: <?php echo $_POST["name"]; ?><br>
-Email: <a href ="<?php echo $_POST["email"]; ?>"><?php echo $_POST["email"]; ?></a><br>
+Email: <a href ="mailto:<?php echo $_POST["email"]; ?>"><?php echo $_POST["email"]; ?></a><br>
 Major: <?php echo $_POST["major"]; ?><br>
 Places you have been: <br>
     <?php 
