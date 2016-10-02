ec1c6f1da52aa9ca1ad22d5de9f0a544cd65343f
Chase Jacobs <chasejacobs@MacBook-Pro.local>
2016-10-01 21:27:12 -0600

    c3

--- web/week03/save.php
+++ web/week03/save.php
@@ -5,31 +5,31 @@ $answer2='';
 $answer3='';
 $answer4='';
 
-$Freshman=0;
-$Sophomore=0;
-$Junior=0;
-$Senior=0;
+$Freshman;
+$Sophomore;
+$Junior;
+$Senior;
     
-$CS=0;
-$WDD=0;
-$CIT=0;
-$SE=0;
-$CE=0;
-$Othermajor=0;
+$CS;
+$WDD;
+$CIT;
+$SE;
+$CE;
+$Othermajor;
 
-$Cplusplus=0;
-$Java=0;
-$Javascript=0;
-$Python=0;
-$C=0;
-$Csharp=0;
-$SQL=0;
-$PHP=0;
-$Ruby=0;
-$Perl=0;
-$Otherlanguage=0;
+$Cplusplus;
+$Java;
+$Javascript;
+$Python;
+$C;
+$Csharp;
+$SQL;
+$PHP;
+$Ruby;
+$Perl;
+$Otherlanguage;
     
-$Yes=0;
-$No=0;
+$Yes;
+$No;
 
 ?>
