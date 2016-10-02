266216b6703d781dda507c4a6ce90cea0e7af7d2
Chase Jacobs <chasejacobs@MacBook-Pro.local>
2016-10-01 20:58:41 -0600

    c3

--- web/week03/results.php
+++ web/week03/results.php
@@ -1,5 +1,62 @@
-<?php 
-    
+<?php
+    include 'save.php';
+    $answer1 = $_POST["year"];
+    echo "The year is " . $answer1 . " and " . $viewed;
+/*
+    if($_POST["year"] == "Freshman"){
+        
+    } elseif ($_POST["year"] == "Sophomore"){
+        
+    } elseif ($_POST["year"] == "Junior"){
+        
+    } elseif ($_POST["year"] == "Senior"){
+        
+    }
+
+    if($_POST["major"] == "CS"){
+        
+    } elseif ($_POST["major"] == "WDD"){
+        
+    } elseif ($_POST["major"] == "CIT"){
+        
+    } elseif ($_POST["major"] == "SE"){
+        
+    } elseif ($_POST["major"] == "CE"){
+        
+    } elseif ($_POST["major"] == "Other"){
+        
+    }
+
+    if($_POST["language"] == "Cplus"){
+        
+    } elseif ($_POST["language"] == "Java"){
+        
+    } elseif ($_POST["language"] == "Javascript"){
+        
+    } elseif ($_POST["language"] == "Python"){
+        
+    } elseif ($_POST["language"] == "C"){
+        
+    } elseif ($_POST["language"] == "Csharp"){
+        
+    } elseif ($_POST["language"] == "SQL"){
+        
+    } elseif ($_POST["language"] == "PHP"){
+        
+    } elseif ($_POST["language"] == "Ruby"){
+        
+    } elseif ($_POST["language"] == "Perl"){
+        
+    } elseif ($_POST["language"] == "Other"){
+        
+    }
+
+    if ($_POST["prof"] == "yes"){
+        
+    } elseif ($_POST["prof"] == "no"){
+        
+    }
+    */
 ?>
 
 <!DOCTYPE html>
--- web/week03/save.php
+++ web/week03/save.php
@@ -5,4 +5,62 @@ $answer2='';
 $answer3='';
 $answer4='';
 
+$Freshman=0;
+$Sophomore=0;
+$Junior=0;
+$Senior=0;
+    
+$CS=0;
+$WDD=0;
+$CIT=0;
+$SE=0;
+$CE=0;
+$Othermajor=0;
+
+$Cplusplus=0;
+$Java=0;
+$Javascript=0;
+$Python=0;
+$C=0;
+$Csharp=0;
+$SQL=0;
+$PHP=0;
+$Ruby=0;
+$Perl=0;
+$Otherlanguage=0;
+    
+$Yes=0;
+$No=0;
+
 ?>
+
+What year are you in school?
+Freshman
+Sophomore
+Junior
+Senior
+
+What is your major?
+CS
+WDD
+CIT
+SE
+CE
+Other
+
+What is your favorite to program in?
+C++
+Java
+Javascript
+Python
+C
+C#
+SQL
+PHP
+Ruby
+Perl
+Other
+
+Have you ever had a proffesional level programming job/internship?
+Yes
+No
