1ef21b1c312a98f96931ec8ae69913be921fd635
Chase Jacobs <chasejacobs@MacBook-Pro.local>
2016-10-15 13:00:26 -0600

    database

---
+++ web/week05/database.sql
@@ -0,0 +1,32 @@
+DROP TABLE item;
+DROP TABLE category;
+
+CREATE TABLE item (
+    id SERIAL PRIMARY KEY NOT NULL,
+    name TEXT NOT NULL,
+    description TEXT NOT NULL,
+    quantity INT NOT NULL,
+    price INT NOT NULL
+);
+
+CREATE TABLE category (
+    id INT PRIMARY KEY NOT NULL,
+    name TEXT NOT NULL,
+    item_id INT REFERENCES item(id) NOT NULL
+);
+
+INSERT INTO item (name, description, quantity, price)
+    VALUES ('Bookshelf', '3 shelves, white', 2, 16);
+    
+    
+INSERT INTO item (name, description, quantity, price)
+    VALUES ('Bedframe', 'Queen, black, wooden', 15, 45);
+    
+    INSERT INTO item (name, description, quantity, price)
+    VALUES ('Desk lamp', '250W bulb, color vaires', 30, 3);
+    
+    INSERT INTO item (name, description, quantity, price)
+    VALUES ('Ceiling fan', '5 blades, black', 3, 35);
+    
+    INSERT INTO item (name, description, quantity, price)
+    VALUES ('Dining Chairs', 'Wooden, black', 64, 8);
