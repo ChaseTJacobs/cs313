61ad9bda3dddb2968fdcdec2bc51b6cae4e052bb
Chase Jacobs <chasejacobs@MacBook-Pro.local>
2016-10-15 21:37:21 -0600

    table

--- web/week05/data.php
+++ web/week05/data.php
@@ -44,6 +44,12 @@ catch (PDOException $ex) {
     <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST">
         <select name="price">
             <option value="all">All prices</option>
+            <option value="0">$0 to $10</option>
+            <option value="11">$11 to $50</option>
+            <option value="51">$51 to $100</option>
+            <option value="101">$101 to $200</option>
+            <option value="201">$201 to $300</option>
+            <option value="301">$301 and above</option>
             <?php
             $query = $db->query('SELECT * FROM item')->fetchAll();
             
@@ -51,9 +57,12 @@ catch (PDOException $ex) {
                 $prices = $_POST['price'];
                 if($prices != 'all'){
                     $query = $db->query("SELECT * FROM item WHERE price='$prices'")->fetchAll();
+                    if ($prices == 11){
+                        echo 'IT IS ELEVEN';
+                    }
                 }
             }
-            
+            /*
             foreach($db->query('SELECT DISTINCT price FROM item')->fetchAll() as $prices){
                 if($_SERVER["REQUEST_METHOD"] == "POST"){
                     if($_POST["price"] == $prices["price"]){
@@ -64,6 +73,7 @@ catch (PDOException $ex) {
                     }
                 }
                 echo '<option value="' . $prices['price'] . '"' . $selected . '>' . $prices['price'] . '</option>';
+                */
             }
             ?>
             <input type="submit" value="Search"/>
@@ -72,6 +82,7 @@ catch (PDOException $ex) {
     </form>
     <?php
         foreach($query as $row){
+            //if ($row['price'])
             echo '<tr>';
             echo '<td>' . $row['name'] . '</td>';
             echo '<td>' . $row['description'] . '</td>';
