1470e56dae13f5a0f1f0e6b46876811c73dd4c80
Chase Jacobs <chasejacobs@MacBook-Pro.local>
2016-10-22 22:38:32 -0600

    first test

--- web/week06/week06.php
+++ web/week06/week06.php
@@ -16,7 +16,7 @@ require('dbconnectionta.php');
 <html>
     <head>
         <link type="text/css" rel="stylesheet" href="../css/main.css" />
-        <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
+        <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
         <script type="text/javascript">
             $(document).ready(function(){
                 $("#addForm").submit(function(e) {
