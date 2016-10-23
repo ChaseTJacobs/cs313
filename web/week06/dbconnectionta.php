184a6e2cb95aaa30dae4b78e9668b0c6dc25d83c
Chase Jacobs <chasejacobs@MacBook-Pro.local>
2016-10-22 22:48:58 -0600

    first test

--- web/week06/dbconnectionta.php
+++ web/week06/dbconnectionta.php
@@ -10,6 +10,7 @@
      $dbName = ltrim($dbopts["path"],'/');
     try {
      $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+        $output = "<script>console.log( 'This ran and worked' );</script>";
     }
     catch (PDOException $ex) {
      print "<p>error: $ex->getMessage() </p>\n\n";
