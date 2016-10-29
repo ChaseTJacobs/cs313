2e5852e87988bc0bd37b536dd5b8820bd1e720f7
Chase Jacobs <chasejacobs@MacBook-Pro.local>
2016-10-28 21:24:26 -0600

    stuff

--- web/week06/week06.php
+++ web/week06/week06.php
@@ -153,12 +153,9 @@ catch (PDOException $ex) {
         echo '<tr>';
         echo '<td>' . $cat['name'] . '</td>';
         echo '</tr>';
-        $query = $db->query("SELECT * FROM item 
-                             INNER JOIN category
-                             on item.category_id=category.id
-                             WHERE category.id=$length")->fetchAll();
+        $query = $db->query("SELECT * FROM item")->fetchAll();
         foreach($query as $row){
-            if ($row['price'] >= $low && $row['price'] <= $high){
+            if ($row['price'] >= $low && $row['price'] <= $high && $row['category_id'] == $length){
                 echo '<tr>';
                 echo '<td>' . $row['name'] . '</td>';
                 echo '<td>' . $row['description'] . '</td>';
