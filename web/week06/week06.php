f35582ba2c0bc87b7b0090578ff970eb565a73e0
Chase Jacobs <chasejacobs@MacBook-Pro.local>
2016-11-04 14:47:49 -0600

    fix

--- web/week06/week06.php
+++ web/week06/week06.php
@@ -232,7 +232,7 @@ catch (PDOException $ex) {
                 $description = $row['description'];
                 $quantity = $row['quantity'];
                 $price = $row['price'];
-                echo '<form action="" method="post" id="form' . $length . $rowID . '">'
+                //echo '<form action="" method="post" id="form' . $length . $rowID . '">'
                 echo '<tr class="row' . $length . '" name="' . $rowID . '">';
                 echo '<td>' . 
                     '<input type="text" class="save"' . 'name="name' . $length . $rowID . '"' . ' value="' . 
@@ -284,7 +284,7 @@ catch (PDOException $ex) {
                     ')"' . 
                     ' value="Save" />' . 
                     '</td>';
-                echo '</tr></form>';
+                echo '</tr>';
                 $rowID = $rowID + 1;
             }
         }
